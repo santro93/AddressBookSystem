240dad596c1aee32b051d8dbe8be9229823d3be4
santro93 <nsantosh435@gmail.com>
2022-03-25 23:09:09 +0530

    add new Contact to Address Book

--- AddressBook.java
+++ AddressBook.java
@@ -1,5 +1,43 @@
 package com.bridgelabz.addressbooksystem;
 
-public class AddressBook {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class AddressBook {
+	ArrayList<Contacts> tempList = new ArrayList<>();
+
+	public void addContact(Scanner sc) {
+		Contacts contact = new Contacts();
+		System.out.print("Enter First Name: ");
+		contact.setFirstName(sc.nextLine());
+		System.out.print("Enter Last Name: ");
+		contact.setLastName(sc.nextLine());
+		System.out.print("Enter Address: ");
+		contact.setAddress(sc.nextLine());
+		System.out.print("Enter City: ");
+		contact.setCity(sc.nextLine());
+		System.out.print("Enter State: ");
+		contact.setState(sc.nextLine());
+		System.out.print("Enter Email Id: ");
+		contact.setEmail(sc.nextLine());
+		System.out.print("Enter Phone number: ");
+		contact.setPhone(sc.nextLine());
+		System.out.print("Enter Zipcode: ");
+		contact.setZip(sc.nextLine());
+		tempList.add(contact);
+	}
+	
+	public void displayContact(){
+		for (Contacts contact:tempList){
+			System.out.println("First name : " +contact.firstName);
+			System.out.println("last name : " +contact.lastName);
+			System.out.println("Address : " +contact.address);
+			System.out.println("City : " +contact.city);
+			System.out.println("State : " +contact.state);
+			System.out.println("Email Id : " +contact.email);
+			System.out.println("Phone number : " +contact.phone);
+			System.out.println("Zipcode : "+contact.zip);
+		}
+	}
 }
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,8 +1,32 @@
 package com.bridgelabz.addressbooksystem;
 
+import java.util.Scanner;
 public class AddressBookMain {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book Program!");
-	}
+		
+		Scanner sc = new Scanner(System.in);
+		AddressBook addressBook = new AddressBook();
+		int option;
+		do {
+			System.out.println("-------------------------------------------------");
+			System.out.println("Press\n1. To add a new contact \n2. To display contact \n3. To exit");
+			option = sc.nextInt();
+			sc.nextLine();
+			switch (option) {
+				case 1:
+					addressBook.addContact(sc);
+					break;
+				case 2:
+					addressBook.displayContact();
+					break;
+				case 3:
+					break;
+				default:
+					System.out.println("Enter a valid option");
+					break;
+				}
+		}
+		while (option != 3);}
 }
